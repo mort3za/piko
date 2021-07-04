047f048dc7b0f1325b050cce19bdfac04bd9ac8c
Morteza Ziyae <m.ziaeemehr@gmail.com>
2021-07-04 15:21:41 +0430

    add pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import { ViteAliases } from "vite-aliases";
 import vue from "@vitejs/plugin-vue";
 
 const viteAliasesOptions = {
   dir: "src",
+  // to update tsconfig paths:
+  // useTypescript: true,
+  // useConfig: true,
 };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteAliases(viteAliasesOptions)],
+  plugins: [vue(), ViteAliases(viteAliasesOptions), VitePWA()],
 });
