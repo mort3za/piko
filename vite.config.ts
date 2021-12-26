4e32c96cb4236c653c8c19ff92065cd5a70c97e1
Morteza Ziyae <m.ziaeemehr@gmail.com>
2021-12-26 23:37:34 +0100

    fix base, store warning

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/piko/",
   resolve: {
     alias: {
       "@pages": path.resolve(__dirname, "./src/pages"),
