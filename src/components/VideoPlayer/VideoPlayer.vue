9b932eae80a2c6fb268e632840bf752975e97261
Morteza <morteza@pepper.com>
2022-04-11 00:29:28 +0200

    pause video outside viewport

--- src/components/VideoPlayer/VideoPlayer.vue
+++ src/components/VideoPlayer/VideoPlayer.vue
@@ -9,18 +9,39 @@
   ></video>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script lang="ts" setup>
+import { MediaEntity } from "twitter-d";
+import { onBeforeUnmount, onMounted, onUnmounted, ref } from "vue";
 
-export default defineComponent({
-  name: "VideoPlayer",
-  props: ["mediaItem"],
-  methods: {
-    getChosenVideo() {
-      return this.mediaItem.video_info.variants.find(
-        (variant: any) => variant.content_type === "video/mp4",
-      ).url;
-    },
+const props = defineProps({
+  mediaItem: {
+    type: Object as () => MediaEntity,
+    required: true,
   },
 });
+
+const video = ref(null as unknown as HTMLVideoElement);
+// pause video when it's not in viewport
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio != 1 && !video.value.paused) {
+        video.value.pause();
+      }
+    });
+  },
+  { threshold: 0 },
+);
+onMounted(() => {
+  observer.observe(video.value);
+});
+onBeforeUnmount(() => {
+  observer.unobserve(video.value);
+});
+
+function getChosenVideo() {
+  // @ts-ignore
+  return props.mediaItem.video_info.variants.find((variant) => variant.content_type === "video/mp4")
+    .url;
+}
 </script>
