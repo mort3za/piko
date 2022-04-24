2094c7c213dbaade04b17829a27b49264fee5ac9
Morteza Ziyae <m.ziaeemehr@gmail.com>
2022-04-25 00:08:46 +0200

    fix go-to another status

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <router-view />
+  <router-view :key="fullPath" />
 </template>
+
+<script lang="ts" setup>
+import { computed } from "@vue/reactivity";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const fullPath = computed(() => route.fullPath.split("#")[0]);
+</script>
