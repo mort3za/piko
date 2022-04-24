0de4a0ada9148865bf253c732a258fde8db192a8
Morteza Ziyae <m.ziaeemehr@gmail.com>
2022-04-25 01:49:12 +0200

    show video length

--- src/components/VideoPlayer/VideoPlayer.vue
+++ src/components/VideoPlayer/VideoPlayer.vue
@@ -1,17 +1,25 @@
 <template>
-  <video
-    class="w-full block"
-    ref="video"
-    :src="getChosenVideo()"
-    :poster="mediaItem.media_url_https"
-    controls
-    :preload="shouldPreload"
-  ></video>
+  <div class="relative">
+    <div v-if="!played" class="absolute top-0 right-0 bg-white bg-opacity-80 px-1 rounded-bl-md">
+      {{ videoLengthDisplay }}
+    </div>
+    <video
+      class="w-full block"
+      ref="video"
+      :src="getChosenVideo()"
+      :poster="mediaItem.media_url_https"
+      controls
+      :preload="shouldPreload"
+      :loop="shouldLoop"
+      v-bind="$attrs"
+      @play="played = true"
+    ></video>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { MediaEntity } from "twitter-d";
-import { onBeforeUnmount, onMounted, onUnmounted, ref } from "vue";
+import { onBeforeUnmount, onMounted, ref } from "vue";
 
 const props = defineProps({
   mediaItem: {
@@ -22,6 +30,11 @@ const props = defineProps({
 
 // todo: check navigator.connection
 const shouldPreload = "none";
+const videoLength = (props.mediaItem?.video_info as any)?.duration_millis;
+const videoLengthDisplay =
+  videoLength && `${Math.floor(videoLength / 1000 / 60)}:${Math.floor((videoLength / 1000) % 60)}`;
+const shouldLoop = videoLength <= 15000;
+const played = ref(false);
 
 const video = ref(null as unknown as HTMLVideoElement);
 // pause video when it's not in viewport
