f8e2949e874bb998022bbf70065d8bb951dc40a9
Morteza Ziyae <m.ziaeemehr@gmail.com>
2022-04-25 01:51:13 +0200

    add dropdown component

---
+++ src/components/Base/DropDown.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="absolute right-0 top-full bg-slate-50 rounded-md py-2 w-max shadow-lg">
+    <li v-for="(item, index) in items" :key="index" class="py-1 px-2 hover:bg-slate-200">
+      <button @click="select(item)">{{ item.text }}</button>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+export type DropdownItem = {
+  text: string;
+  value: string | number;
+};
+defineProps({
+  items: {
+    type: Array as () => Array<DropdownItem>,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["select"]);
+
+function select(item: DropdownItem) {
+  emit("select", item);
+}
+</script>
