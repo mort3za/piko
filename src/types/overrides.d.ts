c72581b8a8703986aaf7b4ec7678612afa4fdd09
Morteza <morteza@pepper.com>
2022-10-13 09:29:23 +0200

    fix type overrides

--- src/types/overrides.d.ts
+++ src/types/overrides.d.ts
@@ -1,14 +1,5 @@
 import { AxiosResponse } from "axios";
 
-declare module "axios" {
-  export interface AxiosResponse {
-    ok: boolean;
-  }
-  export interface AxiosError {
-    status: number;
-  }
-}
-
 declare module "twitter-d" {
   export interface Status {
     text: string;
