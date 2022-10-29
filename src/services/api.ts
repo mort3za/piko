86735ce4349aa0d01986a4b4be0058029eefc7d3
Morteza <morteza@pepper.com>
2022-10-29 23:11:37 +0200

    add refresh-token to every request

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,8 @@ import { Status } from "twitter-d";
 const { VITE_API_ORIGIN, VITE_API_BASE } = import.meta.env;
 const apiUrl = `${VITE_API_ORIGIN}${VITE_API_BASE}`;
 
-export const api = (path: string, options: RequestInit = {}) => {
+export const api = async (path: string, options: RequestInit = {}) => {
+  await refreshToken();
   const headers = new Headers({ "Content-Type": "application/json" });
 
   const req = new Request(`${apiUrl}${path}`, {
@@ -18,6 +19,16 @@ export const api = (path: string, options: RequestInit = {}) => {
     }
     return response;
   });
+};
+
+export const refreshToken = () => {
+  const headers = new Headers({ "Content-Type": "application/json" });
+  const req = new Request(`${apiUrl}${"/refresh-token"}`, {
+    method: "GET",
+    headers,
+    credentials: "include",
+  });
+  return fetch(req);
 };
 
 export const apiLink = (path: string) => `${apiUrl}${path}`;
